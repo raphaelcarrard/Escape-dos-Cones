4e1a2961c599def5664cb944b2eae75d6ca160df
Raphael Carrard <70378163+raphaelcarrard@users.noreply.github.com>
2020-11-08 22:20:42 -0300

    Source Code

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour
+{
+
+    public static MenuScript instance;
+    [SerializeField]
+    public Button comoJogar, fecharPainel;
+    [SerializeField]
+    public GameObject painel;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void AbrePainelDeComoJogar()
+    {
+        painel.SetActive(true);
+    }
+
+    public void FechaPainel()
+    {
+        painel.SetActive(false);
+    }
+
+    public void SairDoJogo()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movimento : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    private PlayerMovimento movePlayer;
+
+    void Start()
+    {
+        movePlayer = GameObject.Find("Player").GetComponent<PlayerMovimento>();
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (gameObject.name == "BotaoEsquerda")
+        {
+            movePlayer.SetarParaEsquerda(true);
+        }
+        else
+        {
+            movePlayer.SetarParaEsquerda(false);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+           movePlayer.PararMovimento();
+    }
+}
